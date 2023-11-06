d9f6b8063008a4e5610671895d9cd2e2c9936dd3
KarenjuNjeru <johnkarenju690@gmail.com>
2023-11-06 13:26:50 -0800

    karenju

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - return file name
- * Return: file name
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf(_FILE_);
-	printf("\n");
+	printf("%s\n", __FILE__);
 	return (0);
 }
+
